943766939b7d08df4e105e502b38a8091281056f
Yohan Koehler <yohan.koehler@aufeminin.com>
2021-11-01 15:51:25 +0100

    feat(typescript) update _app typing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import type { AppProps } from 'next/app'
-import { getLoggedUserId } from '../utils/getLoggedUserId'
-import '../styles/globals.css'
+import type { AppProps } from 'next/app';
+import { getLoggedUserId } from '../utils/getLoggedUserId';
+import '../styles/globals.css';
 
 // Default way to get a logged user
-export const loggedUserId = getLoggedUserId()
+export const loggedUserId = getLoggedUserId();
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default MyApp;
