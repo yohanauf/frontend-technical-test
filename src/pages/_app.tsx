57dc9ae9506cba3699d03e5286b0caaba49cb622
Yohan Koehler <yohan.koehler@aufeminin.com>
2021-11-09 16:57:03 +0100

    feat(app): add dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import type { AppProps } from 'next/app';
 import { getLoggedUserId } from '../utils/getLoggedUserId';
 import '../styles/globals.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 // Default way to get a logged user
 export const loggedUserId = getLoggedUserId();
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
